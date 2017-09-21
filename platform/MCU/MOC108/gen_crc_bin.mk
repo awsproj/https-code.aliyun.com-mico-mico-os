1ff6281dc6ecf98ca65c0498fe967ddfbd8f173f
杨诗伟 <yangsw@mxchip.com>
2017-09-21 17:01:38 +0800

    增加 .ota.bin.xz 的生成。

--- platform/MCU/MOC108/gen_crc_bin.mk
+++ platform/MCU/MOC108/gen_crc_bin.mk
@@ -1,15 +1,19 @@
 
 ifeq ($(HOST_OS),Win32)
 ENCRYPT := "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/encrypt_win.exe"
+XZ 		:= "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/xz_win.exe"
 else  # Win32
 ifeq ($(HOST_OS),Linux32)
 ENCRYPT := "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/encrypt_linux"
+XZ 		:= "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/xz_linux"
 else # Linux32
 ifeq ($(HOST_OS),Linux64)
 ENCRYPT := "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/encrypt_linux"
+XZ 		:= "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/xz_linux"
 else # Linux64
 ifeq ($(HOST_OS),OSX)
 ENCRYPT := "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/encrypt_osx"
+XZ 		:= "$(SOURCE_ROOT)/mico-os/platform/MCU/MOC108/xz_osx"
 else # OSX
 $(error not surport for $(HOST_OS))
 endif # OSX
@@ -23,6 +27,6 @@ OTA_BIN_OUTPUT_FILE := $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.ota$(BIN_OUTPUT
 EXTRA_POST_BUILD_TARGETS += gen_crc_bin
 
 gen_crc_bin:
-	$(eval OUT_MSG := $(shell $(ENCRYPT) $(BIN_OUTPUT_FILE) 0 0 0 0))
+	$(eval OUT_MSG := $(shell $(ENCRYPT) $(BIN_OUTPUT_FILE) 0 0 0 0; $(XZ) --lzma2=dict=32KiB --check=crc32 -k $(CRC_BIN_OUTPUT_FILE)))
 	$(QUIET)$(CP) $(CRC_BIN_OUTPUT_FILE) $(OTA_BIN_OUTPUT_FILE)
 	$(QUIET)$(RM) $(CRC_BIN_OUTPUT_FILE)
